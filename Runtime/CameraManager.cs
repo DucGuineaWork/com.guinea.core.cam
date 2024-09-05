521c456ab0fd769f8ca3427a3bd2cafd9d65882d
DucGuineaWork <127275632+DucGuineaWork@users.noreply.github.com>
2024-09-05 15:21:44 +0700

    - Update CameraManager.cs

--- Runtime/CameraManager.cs
+++ Runtime/CameraManager.cs
@@ -4,13 +4,27 @@ namespace Guinea.Core.Cam
 {
     public class CameraManager: MonoBehaviour
     {
+        [SerializeField]bool m_setMainCameraAtStart;
         private static Camera s_mainCamera;
 
         public static Camera Main=> s_mainCamera;
 
+        void Start()
+        {
+            if(m_setMainCameraAtStart)
+            {
+                s_mainCamera = Camera.main;
+            }
+        }
+
+        public static SetMainCamera(Camera camera)
+        {
+            s_mainCamera = camera
+        }
+
         public static bool WorldToCanvasPosition(Vector3 worldPosition, RectTransform rect, out Vector2 localPoint, Camera cam = null)
         {
-            Vector3 screenPosition = Camera.main.WorldToScreenPoint(worldPosition);
+            Vector3 screenPosition = s_mainCamera.WorldToScreenPoint(worldPosition);
             return RectTransformUtility.ScreenPointToLocalPointInRectangle(
             rect,
             screenPosition,
