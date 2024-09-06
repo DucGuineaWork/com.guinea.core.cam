94e17571dd39ac41c7f0432e58d80f2901622a6f
Pham Duc <ducpt.guinea.work@gmail.com>
2024-09-06 10:18:30 +0700

    * Using List for BecameVisible

--- Runtime/BecameVisibleObjectManager.cs
+++ Runtime/BecameVisibleObjectManager.cs
@@ -6,14 +6,17 @@ namespace Guinea.Core.Cam
 {
     public static class BecameVisibleObjectManager
     {
-        private static HashSet<GameObject> s_visibleObjects = new HashSet<GameObject>();
-        public static HashSet<GameObject> VisibleObjects=>s_visibleObjects;
+        private static List<GameObject> s_visibleObjects = new List<GameObject>();
+        public static List<GameObject> VisibleObjects=>s_visibleObjects;
         public static event Action OnChanged;
 
         public static void OnBecameVisibleEvent(GameObject gameObject)
         {
-            s_visibleObjects.Add(gameObject);
-            OnChanged?.Invoke();
+            if(!s_visibleObjects.Contains(gameObject))
+            {
+                s_visibleObjects.Add(gameObject);
+                OnChanged?.Invoke();
+            }
         }
 
         public static void OnBecameInvisibleEvent(GameObject gameObject)
